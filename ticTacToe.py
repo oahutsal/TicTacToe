101a19df3961d914dfe798d93e61bd4fb367725d
oahutsal <65304757+oahutsal@users.noreply.github.com>
2020-12-05 05:01:53 +0200

    Finally done, all bugs are fixed!

--- ticTacToe.py
+++ ticTacToe.py
@@ -22,7 +22,7 @@ n = False
 #    if i in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
 #        indexes.append(board.index(i))
 #print(indexes)
-#indexes = [44, 50, 56, 124, 130, 56, 204, 210, 216]
+#indexes = [44, 50, 56, 124, 130, 136, 204, 210, 216]
 
 def xo_func():
     print('\nWelcome to the tic tac toe!')
@@ -38,78 +38,75 @@ def xo_func():
     return player_choise, computer_choise
 
 player_choise = xo_func()
+print(''.join(board))
 #player_choise[0] is player, player_choise[1] is computer
-
 def place_func():
     player_place = input('Write the nummer (1-9) to mark this place: ')
     if player_place in chanses:
-        random_num = str(random.choice(chanses))
-        board.insert(board.index(player_place), player_choise[0])
-        board.remove(player_place)
-        board.insert(board.index(random_num), player_choise[1])
-        board.remove(random_num)
-        chanses.remove(player_place)
-        chanses.remove(random_num)
-        print(''.join(board))
-        print(str(chanses))
-        return True
-    elif len(chanses) == 0:
-        a = input('Game over!\nDo you want to continue?[y/n]')
-        return a.upper()
+        if len(chanses) != 0:
+            if player_place in chanses:
+                board.insert(board.index(player_place), player_choise[0])
+                board.remove(player_place)
+                chanses.remove(player_place)
+                if len(chanses) > 0:
+                    random_num = str(random.choice(chanses))
+                    board.insert(board.index(random_num), player_choise[1])
+                    board.remove(random_num)
+                    chanses.remove(random_num)
+                elif len(chanses) == 0:
+                    return False
+            print(''.join(board))
+            print(str(chanses))
     else: 
-        print('ERROR!!!\nOn ly numbers which are free!\nYOU LOSE!')
-        return False
+        print('ERROR!!!\nOnly numbers which are free!\n' + str(chanses))
 
 def combinations_player():
-    if board[204] and board[210] and board[216] == player_choise[0]:
-        return True
-    elif board[124] and board[130] and board[136] == player_choise[0]:
-        return True
-    elif board[44] and board[50] and board[56] == player_choise[0]:
-        return True
-    elif board[204] and board[124] and board[44] == player_choise[0]:
-        return True
-    elif board[210] and board[130] and board[50] == player_choise[0]:
-        return True
-    elif board[136] and board[56] and board[216] == player_choise[0]:
-        return True
-    elif board[204] and board[130] and board[56] == player_choise[0]:
-        return True
-    elif board[44] and board[130] and board[216] == player_choise[0]:
-        return True
-    else: return False
+    if board[204] + board[210] + board[216] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[124] + board[130] + board[136] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[44] + board[50] + board[56] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[204] + board[124] + board[44] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[210] + board[130] + board[50] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[136] + board[56] + board[216] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[204] + board[130] + board[56] == player_choise[0] * 3:
+        return player_choise[0] * 3
+    elif board[44] + board[130] + board[216] == player_choise[0] * 3:
+        return player_choise[0] * 3
 
 def combinations_computer():
-    if board[204] and board[210] and board[216] == player_choise[1]:
-        return True
-    elif board[124] and board[130] and board[136] == player_choise[1]:
-        return True
-    elif board[44] and board[50] and board[56] == player_choise[1]:
-        return True
-    elif board[204] and board[124] and board[44] == player_choise[1]:
-        return True
-    elif board[210] and board[130] and board[50] == player_choise[1]:
-        return True
-    elif board[136] and board[56] and board[216] == player_choise[1]:
-        return True
-    elif board[204] and board[130] and board[56] == player_choise[1]:
-        return True
-    elif board[44] and board[130] and board[216] == player_choise[1]:
-        return True
-    else: return False
-
+    if board[204] + board[210] + board[216] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[124] + board[130] + board[136] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[44] + board[50] + board[56] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[204] + board[124] + board[44] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[210] + board[130] + board[50] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[136] + board[56] + board[216] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[204] + board[130] + board[56] == player_choise[1] * 3:
+        return player_choise[1] * 3
+    elif board[44] + board[130] + board[216] == player_choise[1] * 3:
+        return player_choise[1] * 3
 
 while not n:
     place = place_func()
-    comb_comp = combinations_computer()
-    player_comb = combinations_player()
-    if place == True:
-        continue
-    elif place == False:
-        break
-    elif place == 'Y':
-        chanses = list('123456789')
-        board = list('''
+    if place != False:
+        computer_comb = combinations_computer()
+        player_comb = combinations_player()
+        if player_comb == player_choise[0] * 3:
+            print('Player ' + player_choise[0] + ' win!')
+            a = input('GAME OVER! Do you want to continue?[y/n] ')
+            if a.lower == 'y':
+                chanses = list('123456789')
+                board = list('''
 ___________________
 |     |     |     |
 |  7  |  8  |  9  |
@@ -124,18 +121,61 @@ ___________________
 |     |     |     |
 |-----------------|
 
-        ''')
-        continue
-    elif place == "N":
-        break
-    if comb_comp == True:
-        print('Player ' + player_choise[1] + ' win!')
-        break
-    elif comb_comp == False:
-        continue
-    if player_comb == True:
-        print('Player ' + player_choise[0] + ' win!')
-        break
-    elif player_comb == False:
-        continue
+''')
+                continue
+            elif a.lower() == 'n':
+                break
+            else:
+                break
+        elif computer_comb == player_choise[1] * 3:
+            print('Player ' + player_choise[1] + ' win!')
+            a = input('GAME OVER! Do you want to continue?[y/n] ')
+            if a.lower() == 'y':
+                chanses = list('123456789')
+                board = list('''
+___________________
+|     |     |     |
+|  7  |  8  |  9  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  4  |  5  |  6  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  1  |  2  |  3  |
+|     |     |     |
+|-----------------|
+
+''')
+                continue
+            elif a.lower() == 'n':
+                break
+            else:
+                break
+        else:
+            continue
+    elif place == False:
+        a = input('GAME OVER! Do you want to continue?[y/n] ')
+        if a.lower() == 'y':
+            chanses = list('123456789')
+            board = list('''
+___________________
+|     |     |     |
+|  7  |  8  |  9  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  4  |  5  |  6  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  1  |  2  |  3  |
+|     |     |     |
+|-----------------|
+
+''')
+            continue
+        elif a.lower() == 'n':
+            break
     
